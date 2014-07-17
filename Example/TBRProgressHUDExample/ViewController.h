9c2cf9a109d44518f573bb9e8fd2a3377eee2e3f
Luciano Marisi <lucianomarisi@gmail.com>
2014-07-17 18:24:49 +0100

    Added blank example project

---
+++ Example/TBRProgressHUDExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TBRProgressHUDExample
+//
+//  Created by Luciano Marisi on 17/07/2014.
+//  Copyright (c) 2014 TechBrewers LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
